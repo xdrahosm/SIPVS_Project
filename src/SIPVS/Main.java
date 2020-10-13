b325864496fccb0e5ce0c4aec8894f33c805166c
xdrahosm <xdrahosm3@stuba.sk>
2020-10-13 18:30:27 +0200

    task 1 final changes

--- src/SIPVS/Main.java
+++ src/SIPVS/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("form.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Library system");
         Scene mainScene = new Scene(root, 800, 800);
 
         primaryStage.setScene(mainScene);
