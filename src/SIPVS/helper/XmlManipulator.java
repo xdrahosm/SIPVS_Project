d9c2a435f181d2648c7ee440defc44ccbcdcb6e6
i508057 <jakub.mikolaj@sap.com>
2020-11-22 15:50:17 +0100

    add time stamp functionality

--- src/SIPVS/helper/XmlManipulator.java
+++ src/SIPVS/helper/XmlManipulator.java
@@ -4,8 +4,16 @@ import SIPVS.model.Book;
 import SIPVS.model.Borrow;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import org.bouncycastle.tsp.TSPAlgorithms;
+import org.bouncycastle.tsp.TimeStampRequest;
+import org.bouncycastle.tsp.TimeStampRequestGenerator;
+import org.bouncycastle.tsp.TimeStampResponse;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.Text;
+import org.xml.sax.InputSource;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -22,6 +30,15 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class XmlManipulator {
 	
@@ -89,6 +106,156 @@ public class XmlManipulator {
 			alert.setContentText("ERROR IN XML");
 			alert.showAndWait();
 	    }
+	}
+
+	private Element newXmlElements(Document document, String timestampToken) {
+
+		Element encapsulatedTimeStampElement = document.createElement("xades:EncapsulatedTimeStamp");
+		Element signatureTimestampElement = document.createElement("xades:SignatureTimeStamp");
+		Element unsignedSignaturePropertiesElement = document.createElement("xades:UnsignedSignatureProperties");
+		Element unsignedPropertiesElement = document.createElement("xades:UnsignedProperties");
+
+		unsignedPropertiesElement.appendChild(unsignedSignaturePropertiesElement);
+		unsignedSignaturePropertiesElement.appendChild(signatureTimestampElement);
+		signatureTimestampElement.appendChild(encapsulatedTimeStampElement);
+		Text timestampNode = document.createTextNode(timestampToken);
+		encapsulatedTimeStampElement.appendChild(timestampNode);
+		return unsignedPropertiesElement;
+	}
+
+	public String addTimeStamp(byte[] data) {
+
+		byte request[] = null;
+		String result = null;
+		TimeStampResponse response = null;
+
+		// request
+		TimeStampRequestGenerator requestGenerator = new TimeStampRequestGenerator();
+		TimeStampRequest timeStampRequest = requestGenerator.generate(TSPAlgorithms.SHA256, data);
+		try {
+			request = timeStampRequest.getEncoded();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// connection
+		OutputStream out;
+		HttpURLConnection con;
+		try {
+			URL url = new URL("http://test.ditec.sk/timestampws/TS.aspx");
+			con = (HttpURLConnection) url.openConnection();
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-type", "application/timestamp-query");
+			con.setRequestProperty("Content-length", String.valueOf(request.length));
+			out = con.getOutputStream();
+			out.write(request);
+			out.flush();
+			if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+				throw new IOException("HTTP Error: " + con.getResponseCode() + " - " + con.getResponseMessage());
+			}
+			InputStream in = url.openStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			StringBuilder builder = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+			result = builder.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// response
+		String timestampToken = null;
+		try {
+			response = new TimeStampResponse(Base64.getDecoder().decode(result));
+			timestampToken = new String(Base64.getEncoder().encode(response.getTimeStampToken().getEncoded()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return timestampToken;
+	}
+
+    private StreamResult addToDocument(String signedXml) {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(signedXml)));
+
+            Node signatureValue = document.getElementsByTagName("ds:SignatureValue").item(0);
+            if (signatureValue == null) {
+                System.err.println("SignatureValue is missing");
+            }
+
+            String timeStamp = addTimeStamp(Base64.getEncoder().encodeToString(signatureValue.getTextContent().getBytes()).getBytes());
+
+            Node qualifyingProperties = document.getElementsByTagName("xades:QualifyingProperties").item(0);
+            if (qualifyingProperties == null) {
+                System.err.println("QualifyingProperties is missing");
+            }
+
+            qualifyingProperties.appendChild(newXmlElements(document, timeStamp));
+
+
+            DOMSource source = new DOMSource(document);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            StringWriter outWriter = new StringWriter();
+            StreamResult streamResult = new StreamResult(outWriter);
+            transformer.transform(source, streamResult);
+            return streamResult;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
+	public List<String> getTSFiles(String toTSName){
+		List<String> toTSXMLs = Stream.of(Objects.requireNonNull(new File(toTSName).listFiles()))
+                .filter(file -> !file.isDirectory() && file.getName().contains("xml"))
+                .map(File::getAbsolutePath)
+				.collect(Collectors.toList());
+
+		return toTSXMLs;
+	}
+
+	public String getTSFileName(String fileName){
+		String[] nazovSplit = fileName.split("\\\\");
+		String nazov = nazovSplit[nazovSplit.length - 1];
+		String[] tmp_file_name = nazov.split("/");
+		String file_name = tmp_file_name[tmp_file_name.length-1];
+		return file_name;
+	}
+
+	public void saveXmlWithTS() {
+		String TSDirName = "resources/timeStamped/";
+		String toTSName = "resources/toTimeStamp/";
+		List<String> toTSXMLs = getTSFiles(toTSName);
+		if(toTSXMLs.size() < 1) {
+			System.out.println("No xmls found!");
+		} else {
+			toTSXMLs.forEach((fileName) -> {
+				System.out.println("Reading file: " + fileName);
+
+				String signedXml = null;
+				try {
+					signedXml = new String(Files.readAllBytes(Paths.get(fileName)));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				try (PrintStream out = new PrintStream(new FileOutputStream(TSDirName+"T_"+getTSFileName(fileName)))) {
+					out.print(addToDocument(signedXml).getWriter());
+					System.out.println("Add time stamp to file: " + fileName);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			});
+		}
 	}
 	
 	public void generateXml(Book book, ObservableList<Borrow> borrows, String path) {
